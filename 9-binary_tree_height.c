6a5db87527addff999c108d3c93ad4c8198bfe1e
Travis <ondegotravis@gmail.com>
2024-02-03 16:01:07 +0300

    ISO declaration errors corrected

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,11 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
+	size_t height_right = 0, height_left = 0;
+	size_t height_right = 0, height_left = 0;
+	
 	if (tree == NULL)
 		return (0);
-
-	size_t height_right = 0, height_left = 0;
 
 	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
